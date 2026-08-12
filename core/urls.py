aeeda76b1f6c6d4cf27b7c7345b12775457d6b8b
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-12 11:26:52 -0500

    Add operational Tasks view

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path("", views.overview, name="overview"),
+    path("tasks/", views.tasks_view, name="tasks"),
     path("healthz/", views.healthz, name="healthz"),
 ]
