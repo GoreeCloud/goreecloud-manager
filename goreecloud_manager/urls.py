523a9ff473abc7baec3a05405dcf295f3d910583
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-15 17:46:42 -0500

    Wire shared login throttle into authentication

--- goreecloud_manager/urls.py
+++ goreecloud_manager/urls.py
@@ -4,9 +4,18 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from core.forms import ThrottledAuthenticationForm
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", auth_views.LoginView.as_view(template_name="core/login.html"), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="core/login.html",
+            authentication_form=ThrottledAuthenticationForm,
+        ),
+        name="login",
+    ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("core.urls")),
 ]
