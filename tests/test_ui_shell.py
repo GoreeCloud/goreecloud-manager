a27b74963a849195bcae10f0a62325846474eef9
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-15 01:16:29 -0500

    Expand Glaze UI shell rendering regressions

--- tests/test_ui_shell.py
+++ tests/test_ui_shell.py
@@ -6,15 +6,20 @@ from django.urls import reverse
 
 
 class ManagerShellTests(TestCase):
-    def test_login_exposes_accessible_glaze_ui_shell(self):
+    def test_login_exposes_accessible_private_glaze_ui_shell(self):
         response = self.client.get(reverse("login"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Skip to main content")
         self.assertContains(response, "data-theme-toggle")
         self.assertContains(response, "Authenticated access is required")
-        self.assertContains(response, 'id="main-content"')
+        self.assertContains(response, 'id="main-content" tabindex="-1"')
+        self.assertContains(response, 'data-glaze-ui="manager"')
+        self.assertContains(response, 'content="noindex, nofollow, noarchive"')
+        self.assertContains(response, 'name="referrer" content="same-origin"')
+        self.assertContains(response, "core/img/manager-mark.svg")
+        self.assertContains(response, "core/css/glaze-ui.css")
 
-    def test_overview_marks_active_navigation(self):
+    def test_overview_marks_active_navigation_and_goreecloud_identity(self):
         user = get_user_model().objects.create_user(username="ui-shell-admin")
         self.client.force_login(user)
         response = self.client.get(reverse("overview"))
@@ -22,6 +27,9 @@ class ManagerShellTests(TestCase):
         self.assertContains(response, 'aria-label="Primary"')
         self.assertContains(response, 'aria-current="page"', count=1)
         self.assertContains(response, "data-theme-toggle")
+        self.assertContains(response, 'class="brand-symbol"')
+        self.assertContains(response, "GoreeCloud")
+        self.assertContains(response, "Manager")
 
     def test_tasks_marks_tasks_navigation_as_current(self):
         user = get_user_model().objects.create_user(username="ui-shell-tasks-admin")
@@ -29,3 +37,4 @@ class ManagerShellTests(TestCase):
         response = self.client.get(reverse("tasks"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'href="/tasks/" aria-current="page"')
+        self.assertContains(response, 'data-glaze-ui="manager"')
